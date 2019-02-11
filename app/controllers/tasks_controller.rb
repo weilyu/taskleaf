752fd7adf4451af9255841c05a687b79ff20a058
Wei Lyu <lvwei1001@live.com>
2019-02-11 21:16:16 +0900

    タスク削除

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,7 +25,12 @@ class TasksController < ApplicationController
     task = Task.find(params[:id])
     task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+  end
 
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
 
   private
