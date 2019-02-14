184c3127a01d14d79fbf0c46d6bf53bbe6e4ea26
Wei Lyu <lvwei1001@live.com>
2019-02-14 21:46:42 +0900

    validate name not including comma

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,14 @@
 class Task < ApplicationRecord
+
   validates_presence_of :name
 
   validates_length_of :name, maximum: 30
+
+  validate :validate_name_not_including_comma
+
+  private
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
