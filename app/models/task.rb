e62b44463f91a302c4d451ad45b9cf798a8b1c42
Wei Lyu <lvwei1001@live.com>
2019-02-15 23:35:35 +0900

    add before validation to name

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,9 +6,16 @@ class Task < ApplicationRecord
 
   validate :validate_name_not_including_comma
 
+  before_validation :set_nameless_name
+
   private
 
   def validate_name_not_including_comma
     errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
   end
+
+  def set_nameless_name
+    self.name = '名前無し' if name.blank?
+  end
+
 end
