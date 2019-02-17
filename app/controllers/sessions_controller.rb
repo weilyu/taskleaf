0a35652aaa5985667ffd2cb65985473d11c032f1
Wei Lyu <lvwei1001@live.com>
2019-02-17 23:00:19 +0900

    add logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,6 +13,12 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    reset_session
+    redirect_to root_path, notice: 'ログアウトしました。'
+    
+  end
+
   private
 
   def session_params
