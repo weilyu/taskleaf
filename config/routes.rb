d6a51eb10e58647c03129217e51b1b1278e07438
Wei Lyu <lvwei1001@live.com>
2019-02-17 22:43:43 +0900

    add user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
   namespace :admin do
     resources :users
   end
+
   root to: 'tasks#index'
+
   resources :tasks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
