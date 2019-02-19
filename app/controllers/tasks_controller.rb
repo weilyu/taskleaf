41e0cc10f890727f6a2d939ab3bf4c69ff04b749
Wei Lyu <lvwei1001@live.com>
2019-02-19 22:45:11 +0900

    render edit when validation fail on update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -26,8 +26,11 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
+    if @task.update(task_params)
+      redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
+    else
+      render :edit
+    end
   end
 
   def destroy
