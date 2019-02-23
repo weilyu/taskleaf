ae2cc3c4d5bb602a19556ed24d71f2a6504baccb
Wei Lyu <lvwei1001@live.com>
2019-02-23 23:23:01 +0900

    translate test case

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-describe 'task management', type: :system do
-  describe 'index' do
+describe 'タスク管理機能', type: :system do
+  describe '一覧表示機能' do
 
     before do
       user_a = FactoryBot.create(:user, name: 'userA', email: 'a@example.com')
       FactoryBot.create(:task, name: 'first task', user: user_a)
     end
 
-    context 'when user A logged in' do
+    context 'ユーザAがログインしているとき' do
 
       before do
         visit login_path
@@ -17,13 +17,13 @@ describe 'task management', type: :system do
         click_button 'ログインする'
       end
 
-      it 'task displayed' do
+      it 'ユーザAが作成したタスクが表示される' do
         expect(page).to have_content 'first task'
       end
 
     end
 
-    context 'when user B logged in' do
+    context 'ユーザBがログインしているとき' do
       before do
         FactoryBot.create(:user, name: 'userB', email: 'b.example.com')
         visit login_path
@@ -32,7 +32,7 @@ describe 'task management', type: :system do
         click_button 'ログインする'
       end
 
-      it 'task not displayed' do
+      it 'ユーザAが作成したタスクが表示されない' do
         expect(page).to have_no_content 'first task'
       end
 
