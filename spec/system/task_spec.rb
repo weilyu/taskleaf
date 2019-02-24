16298531d02c2da70615210d76ba9202b15021c2
Wei Lyu <lvwei1001@live.com>
2019-02-24 22:22:05 +0900

    詳細表示テスト追加

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
+
+  let(:user_a) {FactoryBot.create(:user, name: 'userA', email: 'a@example.com')}
+  let(:user_b) {FactoryBot.create(:user, name: 'userB', email: 'b.example.com')}
+  let!(:task_a) {FactoryBot.create(:task, name: 'first task', user: user_a)}
+
+  before do
+    visit login_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログインする'
+  end
+
   describe '一覧表示機能' do
-
-    let(:user_a) {FactoryBot.create(:user, name: 'userA', email: 'a@example.com')}
-    let(:user_b) {FactoryBot.create(:user, name: 'userB', email: 'b.example.com')}
-
-    before do
-      FactoryBot.create(:task, name: 'first task', user: user_a)
-      visit login_path
-      fill_in 'メールアドレス', with: login_user.email
-      fill_in 'パスワード', with: login_user.password
-      click_button 'ログインする'
-    end
 
     context 'ユーザAがログインしているとき' do
 
@@ -29,6 +30,23 @@ describe 'タスク管理機能', type: :system do
 
       it 'ユーザAが作成したタスクが表示されない' do
         expect(page).to have_no_content 'first task'
+      end
+
+    end
+
+  end
+
+  describe '詳細表示機能' do
+
+    context 'ユーザAがログインしているとき' do
+      let(:login_user) {user_a}
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it 'ユーザAが作成したタスクが表示される' do
+        expect(page).to have_content 'first task'
       end
 
     end
