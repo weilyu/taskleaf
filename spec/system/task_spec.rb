4ab4ca0eae08ccd173cf15852be55376c7e526ac
Wei Lyu <lvwei1001@live.com>
2019-02-24 22:09:31 +0900

    use "let" to decrease duplicated code

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -3,19 +3,20 @@ require 'rails_helper'
 describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
 
+    let(:user_a) {FactoryBot.create(:user, name: 'userA', email: 'a@example.com')}
+    let(:user_b) {FactoryBot.create(:user, name: 'userB', email: 'b.example.com')}
+
     before do
-      user_a = FactoryBot.create(:user, name: 'userA', email: 'a@example.com')
       FactoryBot.create(:task, name: 'first task', user: user_a)
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログインする'
     end
 
     context 'ユーザAがログインしているとき' do
 
-      before do
-        visit login_path
-        fill_in 'メールアドレス', with: 'a@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+      let(:login_user) {user_a}
 
       it 'ユーザAが作成したタスクが表示される' do
         expect(page).to have_content 'first task'
@@ -24,13 +25,7 @@ describe 'タスク管理機能', type: :system do
     end
 
     context 'ユーザBがログインしているとき' do
-      before do
-        FactoryBot.create(:user, name: 'userB', email: 'b.example.com')
-        visit login_path
-        fill_in 'メールアドレス', with: 'b@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+      let(:login_user) {user_b}
 
       it 'ユーザAが作成したタスクが表示されない' do
         expect(page).to have_no_content 'first task'
